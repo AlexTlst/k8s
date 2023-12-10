9b71bd97463f200b88b49c0f2520cfbff59ef0af
AlexT <aaa@bbb.ccc>
2023-12-10 08:44:49 -0800

    jenkins pipeline last revision tested

--- k8S_AWS_infra/Jenkins/instance_setup.groovy
+++ k8S_AWS_infra/Jenkins/instance_setup.groovy
@@ -16,6 +16,7 @@ pipeline {
                                   sparseCheckoutPaths: [[path: 'k8S_AWS_infra/TF']]
                               ]],
                               userRemoteConfigs: [[
+                                  credentialsId: 'access_to_git',
                                   url: 'https://github.com/AlexTlst/k8s.git'
                               ]]
                     ])
@@ -26,6 +27,7 @@ pipeline {
         stage('Terraform Plan') {
             steps {
                 sh '''
+                cd ./k8S_AWS_infra/TF
                 echo "yes" | terraform init
                 terraform plan -out=terraform.tfplan
                 '''
@@ -44,10 +46,10 @@ pipeline {
         stage('Terraform Apply') {
             steps {
                 sh '''
+                cd ./k8S_AWS_infra/TF
                 terraform apply terraform.tfplan
                 '''
             }
         }
- 
     }
 }
--- k8S_AWS_infra/Jenkins/instance_tf_destroy.groovy
+++ k8S_AWS_infra/Jenkins/instance_tf_destroy.groovy
@@ -3,6 +3,7 @@ pipeline {
     tools {
         terraform 'tf1.6'
     }
+    
     stages {
         stage('Sparse Checkout') {
             steps {
@@ -15,6 +16,7 @@ pipeline {
                                   sparseCheckoutPaths: [[path: 'k8S_AWS_infra/TF']]
                               ]],
                               userRemoteConfigs: [[
+                                  credentialsId: 'access_to_git',
                                   url: 'https://github.com/AlexTlst/k8s.git'
                               ]]
                     ])
@@ -25,7 +27,7 @@ pipeline {
         stage('Terraform Initialize and Plan Destroy') {
             steps {
                 sh '''
-                cd ./TF
+                cd ./k8S_AWS_infra/TF
                 terraform init
                 terraform plan -destroy -out=destroyplan.tfplan
                 '''
@@ -41,7 +43,7 @@ pipeline {
         stage('Terraform Apply Destroy') {
             steps {
                 sh '''
-                cd ./TF
+                cd ./k8S_AWS_infra/TF
                 terraform apply destroyplan.tfplan
                 '''
             }
